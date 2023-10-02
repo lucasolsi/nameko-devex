0cc4f21af742087c067563d57064e305cc577df1
Lucas Oliveira Silva <lucasolsi@gmail.com>
2023-10-02 16:34:52 -0300

    Caching service

---
+++ caching/cache_service.py
@@ -0,0 +1,19 @@
+import redis
+import json
+
+class CacheService:
+    name = "cache_service"
+
+    def __init__(self):
+        self.redis_client = redis.StrictRedis(host='localhost', port=6379)
+
+    def cache_data(self, key, value, expiration=None):
+        serialized_value = json.dumps(value)
+        self.redis_client.set(key, serialized_value, ex=expiration)
+
+    def retrieve_cached_data(self, key):
+        cached_data = self.redis_client.get(key)
+        if cached_data is not None:
+            return json.loads(cached_data)
+        
+        return None
